1a161d839545e09803262bf658d0009726e8aea0
IsaiasQB <isaias.qb@hotmail.com>
2023-03-31 21:48:47 -0400

    testing inquirer qith a simple question for the user

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
+const inquirer = require('inquirer');
 const fs = require('fs');
-const generatePage = require('./src/page-template.js')
+// const generatePage = require('./src/page-template.js')
 
-const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(name, github, profession);
 
-const [name, github, profession] = profileDataArgs;
+// fs.writeFile('index.html', pageHTML, err => {
+//   if (err) throw err;
 
-
-fs.writeFile('index.html', generatePage(name, github, profession), err => {
-  if (err) throw err;
-
-  console.log('Portfolio completed. Checkout index.html')
-});
+//   console.log('Portfolio completed. Checkout index.html')
+// });
 
 // node app 'Adrian' 'isaiasqb' 'Web Developer'
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: `▶  What is your name?`
+    }
+  ])
+  .then(answers => console.log(answers));
+
+
