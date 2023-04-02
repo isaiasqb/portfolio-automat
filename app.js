02db44714d8e0fdd315de3cc1f8de6ffa93add05
IsaiasQB <isaias.qb@hotmail.com>
2023-04-02 00:25:03 -0400

    added a second set of questions related to project description

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
+// const fs = require('fs');
 // const generatePage = require('./src/page-template.js')
 
 // const pageHTML = generatePage(name, github, profession);
@@ -12,14 +12,78 @@ const fs = require('fs');
 
 // node app 'Adrian' 'isaiasqb' 'Web Developer'
 
-inquirer
-  .prompt([
+
+const promptUserQuestions = () => {
+  return inquirer.prompt([
     {
       type: 'input',
       name: 'name',
-      message: `▶  What is your name?`
+      message: 'What is your name?'
+    },
+    {
+      type: 'input',
+      name: 'profession',
+      message: 'What is your current job title?'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub username:'
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:'
     }
-  ])
-  .then(answers => console.log(answers));
+  ]);
+};
 
+const promptProjectQuestions = () => {
+  console.log(`
+  ==================
+  Add a New Project
+  ==================
+  `);
 
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: 'name',
+      message: 'What is the name of this proiject?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please describe the project (required)'
+    },
+    {
+      type: 'checkbox',
+      name: 'language',
+      message: 'Which features and languages were used to build this project?',
+      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node', 'Git Projects', 'OOP']
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Please include the link to the project (required)'
+    },
+    {
+      type: 'confirm',
+      name: 'feature',
+      message: 'Would you like to have this project in the featured section?',
+      default: false
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddProject',
+      message: 'Would you like to enter another Project?',
+      default: false
+    },
+  ]);
+};
+
+//we're calling a function that returns the result of inquire.prompt, which is a Promise
+promptUserQuestions()
+  .then(answers => console.log(answers))
+  .then(promptProjectQuestions)
+  .then(projectAnswers => console.log(projectAnswers));
